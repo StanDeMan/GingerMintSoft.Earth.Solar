8dd83a1faed694453abf27310258fedd5bb03d50
StanDeMan <StanislausE@gmx.de>
2024-12-06 22:59:42 +0100

    Prefill time zone offset

--- GingerMintSoft.Earth.Solar/PowerPlant/Location.cs
+++ GingerMintSoft.Earth.Solar/PowerPlant/Location.cs
@@ -7,7 +7,7 @@ public class Location()
     public int Altitude { get; set; }
     public double Latitude { get; set; }
     public double Longitude { get; set; }
-    public TimeSpan TimeZoneOffset { get; set; }
+    public TimeSpan TimeZoneOffset { get; set; } = DateTimeOffset.Now.Offset;
 
     public Calculate Calculate { get; set; } = new Calculate();
     public List<Roof> Roofs { get; set; } = [];
