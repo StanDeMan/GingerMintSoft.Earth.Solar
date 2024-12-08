bcc76f47ece597cb4e622140db356395eb087f48
StanDeMan <StanislausE@gmx.de>
2024-12-08 22:13:02 +0100

    Implement roof collection for multiple pv generators

---
+++ GingerMintSoft.Earth.Solar/PowerPlant/Roofs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GingerMintSoft.Earth.Solar.PowerPlant
+{
+    public class Roofs
+    {
+        public List<Roof>? Roof { get; set; }
+    }
+}
