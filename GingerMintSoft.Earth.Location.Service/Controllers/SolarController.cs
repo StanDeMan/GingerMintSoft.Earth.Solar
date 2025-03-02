17766a4d9cf4d0e532e8e46ceaac04d0855a7caf
StanDeMan <StanislausE@gmx.de>
2025-03-02 23:26:05 +0100

    Add solar controller

---
+++ GingerMintSoft.Earth.Location.Service/Controllers/SolarController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GingerMintSoft.Earth.Location.Service.Controllers
+{
+    [Route("Solar")]
+    public class SolarController(ILogger<SolarController> logger) : Controller
+    {
+        private readonly ILogger<SolarController> _logger = logger;
+
+        //[HttpGet(Name = "Powerplant")]
+        //public IEnumerable<Solar> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new Solar
+        //        {
+        //            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //            TemperatureC = Random.Shared.Next(-20, 55),
+        //            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //        })
+        //        .ToArray();
+        //}
+
+        //[HttpPost]
+        //[Route("Users/{userId}/Addresses")]
+        //public async Task<ActionResult> Post(Guid userId, [FromBody] AddressDto addressData)
+        //{
+        //    try
+        //    {
+        //        addressData.UserId = userId;
+        //        await new User(userId).Addresses.Address.Create(addressData);
+        //        return new CreatedResult($"/HelpingHands/Users/{userId}/Addresses", addressData);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        return Conflict($"Cannot create address: {e}");
+        //    }
+        //}
+    }
+}
