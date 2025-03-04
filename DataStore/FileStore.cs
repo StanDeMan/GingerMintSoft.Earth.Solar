6fa4ff0fd518552e30bd272f967644692785e994
StanDeMan <StanislausE@gmx.de>
2025-03-04 19:44:31 +0100

    Flat json database

--- DataStore/FileStore.cs
+++ DataStore/FileStore.cs
@@ -3,200 +3,199 @@ using Serilog;
 using Newtonsoft.Json.Linq;
 using JsonFlatFileDataStore;
 
-namespace DataBase
+namespace DataBase;
+
+public class FileStore : IDisposable, IFileStore
 {
-    public class FileStore : IDisposable
+    private const string DbName = "database";
+    private const string CollectionName = "plants";
+
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")] 
+    public bool IsRunning { get; set; }
+
+    private string? DataBaseName { get; } = DbName;
+
+    private DataStore? DataStore { get; }
+
+    /// <summary>
+    /// Flat Json file data store
+    /// </summary>
+    /// <param name="dataBaseName">Name of file to create</param>
+    public FileStore(string? dataBaseName = null)
     {
-        private const string DbName = "database";
-        private const string CollectionName = "plants";
-
-        [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Global")] 
-        public bool IsRunning { get; set; }
-
-        private string? DataBaseName { get; } = DbName;
-
-        private DataStore? DataStore { get; }
-
-        /// <summary>
-        /// Flat Json file data store
-        /// </summary>
-        /// <param name="dataBaseName">Name of file to create</param>
-        public FileStore(string? dataBaseName = null)
+        try
         {
-            try
-            {
-                // Generate store with upper camel case Json file
-                // and reload before reading collection
-                DataStore = dataBaseName == null 
-                    ? new DataStore($@"{DataBaseName}.json", false, null, true) 
-                    : new DataStore($@"{dataBaseName}.json", false, null, true);
+            // Generate store with upper camel case Json file
+            // and reload before reading collection
+            DataStore = dataBaseName == null 
+                ? new DataStore($@"{DataBaseName}.json", false, null, true) 
+                : new DataStore($@"{dataBaseName}.json", false, null, true);
 
-                DataBaseName = dataBaseName ?? DataBaseName;
+            DataBaseName = dataBaseName ?? DataBaseName;
 
-                Log.Logger = new LoggerConfiguration()
-                    .MinimumLevel.Warning()
-                    .WriteTo.File(@$"logs\{DataBaseName}.log", rollingInterval: RollingInterval.Day)
-                    .CreateLogger();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Warning()
+                .WriteTo.File(@$"logs\{DataBaseName}.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
 
-                IsRunning = true;
-            }
-            catch (Exception e)
-            {
-                Log.Error($"DataBase.File initializing: {e}");
-
-                IsRunning = false;
-            }
+            IsRunning = true;
         }
-
-        /// <summary>
-        /// Insert json 
-        /// </summary>
-        /// <param name="json">This entity</param>
-        /// <returns>true if inserted</returns>
-        public async Task<bool> InsertAsync(string json)
+        catch (Exception e)
         {
-            try
-            {
-                var plant = JToken.Parse(json);
-                var collection = DataStore?.GetCollection(CollectionName);
+            Log.Error($"DataBase.File initializing: {e}");
 
-                // check if entry stored
-                var entry = await Task.Run(() => collection?
-                    .AsQueryable()
-                    .Count(d => d.PlantId == plant.Value<string>("PlantId")));
+            IsRunning = false;
+        }
+    }
 
-                // only one entry allowed
-                return entry == 0 && await collection?.InsertOneAsync(plant)!;
-            }
-            catch (Exception e)
-            {
-                Log.Error($"DataBase.File.InsertAsync: {e}");
+    /// <summary>
+    /// Insert json 
+    /// </summary>
+    /// <param name="json">This entity</param>
+    /// <returns>true if inserted</returns>
+    public async Task<bool> InsertAsync(string json)
+    {
+        try
+        {
+            var plant = JToken.Parse(json);
+            var collection = DataStore?.GetCollection(CollectionName);
+
+            // check if entry stored
+            var entry = await Task.Run(() => collection?
+                .AsQueryable()
+                .Count(d => d.PlantId == plant.Value<string>("PlantId")));
+
+            // only one entry allowed
+            return entry == 0 && await collection?.InsertOneAsync(plant)!;
+        }
+        catch (Exception e)
+        {
+            Log.Error($"DataBase.File.InsertAsync: {e}");
                 
-                return false;
-            }
+            return false;
         }
+    }
 
-        /// <summary>
-        /// Read json
-        /// </summary>
-        /// <param name="json">Read this entity</param>
-        /// <returns>Json as dynamic</returns>
-        public async Task<dynamic?> ReadAsync(string json)
+    /// <summary>
+    /// Read json
+    /// </summary>
+    /// <param name="json">Read this entity</param>
+    /// <returns>Json as dynamic</returns>
+    public async Task<dynamic?> ReadAsync(string json)
+    {
+        try
         {
-            try
-            {
-                var plant = JToken.Parse(json);
-                var collection = DataStore?.GetCollection(CollectionName);
+            var plant = JToken.Parse(json);
+            var collection = DataStore?.GetCollection(CollectionName);
                 
-                return await Task.Run(() => collection?
-                    .AsQueryable()
-                    .FirstOrDefault(d => d.PlantId == plant.Value<string>("PlantId")));
-            }
-            catch (Exception e)
-            {
-                Log.Error($"DataBase.File.ReadAsync: {e}");
-
-                return null;
-            }
+            return await Task.Run(() => collection?
+                .AsQueryable()
+                .FirstOrDefault(d => d.PlantId == plant.Value<string>("PlantId")));
         }
-
-        /// <summary>
-        /// Read json
-        /// </summary>
-        /// <param name="plantId">Search for PlantId</param>
-        /// <returns>Json as dynamic</returns>
-        public async Task<dynamic?> ReadByPlantIdAsync(string plantId)
+        catch (Exception e)
         {
-            try
-            {
-                var collection = DataStore?.GetCollection(CollectionName);
+            Log.Error($"DataBase.File.ReadAsync: {e}");
+
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Read json
+    /// </summary>
+    /// <param name="plantId">Search for PlantId</param>
+    /// <returns>Json as dynamic</returns>
+    public async Task<dynamic?> ReadByPlantIdAsync(string plantId)
+    {
+        try
+        {
+            var collection = DataStore?.GetCollection(CollectionName);
                 
-                return await Task.Run(() => collection?
-                    .AsQueryable()
-                    .FirstOrDefault(d => d.PlantId == plantId));
-            }
-            catch (Exception e)
-            {
-                Log.Error($"DataBase.File.ReadByPlantIdAsync: {e}");
-
-                return null;
-            }
+            return await Task.Run(() => collection?
+                .AsQueryable()
+                .FirstOrDefault(d => d.PlantId == plantId));
         }
-
-        /// <summary>
-        /// Update json
-        /// </summary>
-        /// <param name="json">This entity</param>
-        /// <returns>true id updated</returns>
-        public async Task<bool> UpdateAsync(string json)
+        catch (Exception e)
         {
-            try
-            {
-                var plant = JToken.Parse(json);
-                var collection = DataStore?.GetCollection(CollectionName);
+            Log.Error($"DataBase.File.ReadByPlantIdAsync: {e}");
 
-                return await collection?
-                    .UpdateOneAsync(d => d.PlantId == plant.Value<string>("PlantId"), plant)!;
-            }
-            catch (Exception e)
-            {
-                Log.Error($"DataBase.File.UpdateAsync: {e}");
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Update json
+    /// </summary>
+    /// <param name="json">This entity</param>
+    /// <returns>true id updated</returns>
+    public async Task<bool> UpdateAsync(string json)
+    {
+        try
+        {
+            var plant = JToken.Parse(json);
+            var collection = DataStore?.GetCollection(CollectionName);
+
+            return await collection?
+                .UpdateOneAsync(d => d.PlantId == plant.Value<string>("PlantId"), plant)!;
+        }
+        catch (Exception e)
+        {
+            Log.Error($"DataBase.File.UpdateAsync: {e}");
                 
-                return false;
-            }
+            return false;
         }
+    }
 
-        /// <summary>
-        /// Delete json
-        /// </summary>
-        /// <param name="json">This entity</param>
-        /// <returns>true if deleted</returns>
-        public async Task<bool> DeleteAsync(string json)
+    /// <summary>
+    /// Delete json
+    /// </summary>
+    /// <param name="json">This entity</param>
+    /// <returns>true if deleted</returns>
+    public async Task<bool> DeleteAsync(string json)
+    {
+        try
         {
-            try
-            {
-                var plant = JToken.Parse(json);
-                var collection = DataStore?.GetCollection(CollectionName);
+            var plant = JToken.Parse(json);
+            var collection = DataStore?.GetCollection(CollectionName);
 
-                return await collection?
-                    .DeleteOneAsync(d => d.PlantId == plant.Value<string>("PlantId"))!;
-            }
-            catch (Exception e)
-            {
-                Log.Error($"DataBase.File.DeleteAsync: {e}");
+            return await collection?
+                .DeleteOneAsync(d => d.PlantId == plant.Value<string>("PlantId"))!;
+        }
+        catch (Exception e)
+        {
+            Log.Error($"DataBase.File.DeleteAsync: {e}");
                 
-                return false;
-            }
+            return false;
         }
+    }
 
-        /// <summary>
-        /// Delete json
-        /// </summary>
-        /// <param name="plantId">Search for plantId</param>
-        /// <returns>true if deleted</returns>
-        public async Task<bool> DeleteByPlantIdAsync(string plantId)
+    /// <summary>
+    /// Delete json
+    /// </summary>
+    /// <param name="plantId">Search for plantId</param>
+    /// <returns>true if deleted</returns>
+    public async Task<bool> DeleteByPlantIdAsync(string plantId)
+    {
+        try
         {
-            try
-            {
-                var collection = DataStore?.GetCollection(CollectionName);
+            var collection = DataStore?.GetCollection(CollectionName);
 
-                return await collection?
-                    .DeleteOneAsync(d => d.PlantId == plantId)!;
-            }
-            catch (Exception e)
-            {
-                Log.Error($"DataBase.File.DeleteByPlantIdAsync: {e}");
+            return await collection?
+                .DeleteOneAsync(d => d.PlantId == plantId)!;
+        }
+        catch (Exception e)
+        {
+            Log.Error($"DataBase.File.DeleteByPlantIdAsync: {e}");
                 
-                return false;
-            }
+            return false;
         }
+    }
 
-        /// <summary>
-        /// Dispose file store
-        /// </summary>
-        public void Dispose()
-        {
-            DataStore?.Dispose();
-        }
+    /// <summary>
+    /// Dispose file store
+    /// </summary>
+    public void Dispose()
+    {
+        DataStore?.Dispose();
     }
 }
---
+++ DataStore/IFileStore.cs
@@ -0,0 +1,54 @@
+﻿namespace DataBase
+{
+    public interface IFileStore
+    {
+        bool IsRunning { get; set; }
+
+        /// <summary>
+        /// Insert json 
+        /// </summary>
+        /// <param name="json">This entity</param>
+        /// <returns>true if inserted</returns>
+        Task<bool> InsertAsync(string json);
+
+        /// <summary>
+        /// Read json
+        /// </summary>
+        /// <param name="json">Read this entity</param>
+        /// <returns>Json as dynamic</returns>
+        Task<dynamic?> ReadAsync(string json);
+
+        /// <summary>
+        /// Read json
+        /// </summary>
+        /// <param name="plantId">Search for PlantId</param>
+        /// <returns>Json as dynamic</returns>
+        Task<dynamic?> ReadByPlantIdAsync(string plantId);
+
+        /// <summary>
+        /// Update json
+        /// </summary>
+        /// <param name="json">This entity</param>
+        /// <returns>true id updated</returns>
+        Task<bool> UpdateAsync(string json);
+
+        /// <summary>
+        /// Delete json
+        /// </summary>
+        /// <param name="json">This entity</param>
+        /// <returns>true if deleted</returns>
+        Task<bool> DeleteAsync(string json);
+
+        /// <summary>
+        /// Delete json
+        /// </summary>
+        /// <param name="plantId">Search for plantId</param>
+        /// <returns>true if deleted</returns>
+        Task<bool> DeleteByPlantIdAsync(string plantId);
+
+        /// <summary>
+        /// Dispose file store
+        /// </summary>
+        void Dispose();
+    }
+}
