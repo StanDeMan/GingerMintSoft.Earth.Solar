2e5a869a04eb1eddf90f3f52740bdaa37145ea3d
StanDeMan <StanislausE@gmx.de>
2025-03-05 20:18:48 +0100

    First controller draft

--- GingerMintSoft.Earth.Location.Service/Controllers/SolarController.cs
+++ GingerMintSoft.Earth.Location.Service/Controllers/SolarController.cs
@@ -1,5 +1,4 @@
 ﻿using DataBase;
-using GingerMintSoft.Earth.Location.Service.Data;
 using GingerMintSoft.Earth.Location.Solar;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -14,51 +13,18 @@ namespace GingerMintSoft.Earth.Location.Service.Controllers
 
         private PowerPlant? _powerPlant;
 
-        [HttpPost]
-        [Route("Powerplant/")]
-        public async Task<ActionResult> PostPowerplant([FromBody] Installations? installations)
+        [HttpGet]
+        [Route("Powerplants/{plantId}")]
+        public async Task<ActionResult> PostPowerplant(string plantId)
         {
             try
             {
-                if (installations == null) return BadRequest("No data provided");
-                if (installations.Powerplants == null) return BadRequest("No power plants provided");
-
-                var plants = installations.Powerplants;
-
-                foreach (var plant in plants)
-                {
-                    var powerPlant = new PowerPlant(
-                        plant.Name,
-                        plant.Altitude,             // Höhe über NN in Metern
-                        plant.Latitude,             // Breitengrad in Dezimalgrad
-                        plant.Longitude             // Längengrad in Dezimalgrad
-                    );
-                }
-
-                return new CreatedResult($"/Powerplant", installations);
+                return new CreatedResult(@$"/Powerplants/{plantId}", 
+                    await _plantStore.ReadByPlantIdAsync(plantId));
             }
             catch (Exception e)
             {
-                var error = $"Cannot create power plant: {e}";
-                _logger.LogError(error);
-
-                return Conflict(error);
-            }
-        }
-
-        [HttpPost]
-        [Route("Powerplant/Roofs")]
-        public async Task<ActionResult> PostRoofs([FromBody] Service.Data.Roofs? roofs)
-        {
-            try
-            {
-                if (roofs == null) return BadRequest("No data provided");
-
-                return new CreatedResult($"/Powerplant/Roofs", roofs);
-            }
-            catch (Exception e)
-            {
-                var error = $"Cannot create power plant roof(s): {e}";
+                var error = $"Cannot read power plant: {e}";
                 _logger.LogError(error);
 
                 return Conflict(error);
@@ -66,14 +32,41 @@ namespace GingerMintSoft.Earth.Location.Service.Controllers
         }
 
         [HttpGet]
-        [Route("Powerplant/Yield/ForDay")]
-        public async Task<ActionResult> GetPowerplantData([FromQuery] DateTime? day)
+        [Route("Powerplants/{plantId}/Energy/ForDay")]
+        public async Task<ActionResult> GetEnergyForDay(string plantId, [FromQuery] DateTime? day)
         {
             try
             {
                 _powerPlant = JsonConvert
                     .DeserializeObject<PowerPlant>(JsonConvert
-                        .SerializeObject(await _plantStore.ReadByPlantIdAsync("WxHCombjzTaw")));
+                        .SerializeObject(await _plantStore.ReadByPlantIdAsync(plantId)));
+
+                _powerPlant.ExecCalculation();
+
+                await Task.Run(() => _powerPlant.Calculate.RadiationOnTiltedPanel(ProcessDate(day)));
+                _powerPlant.EnergyEarning = _powerPlant.EnergyOverDay();
+
+                return new CreatedResult($@"Powerplants/{plantId}/Energy/ForDay?day={day}", 
+                    JsonConvert.SerializeObject(_powerPlant.EnergyEarning));
+            }
+            catch (Exception e)
+            {
+                var error = $"Cannot calculate energy for a day: {e}";
+                _logger.LogError(error);
+
+                return Conflict(error);
+            }
+        }
+
+        [HttpGet]
+        [Route("Powerplants/{plantId}/Power/ForDay")]
+        public async Task<ActionResult> GetPowerForDay(string plantId, [FromQuery] DateTime? day)
+        {
+            try
+            {
+                _powerPlant = JsonConvert
+                    .DeserializeObject<PowerPlant>(JsonConvert
+                        .SerializeObject(await _plantStore.ReadByPlantIdAsync(plantId)));
 
                 _powerPlant.ExecCalculation();
 
@@ -84,19 +77,32 @@ namespace GingerMintSoft.Earth.Location.Service.Controllers
                     new DateTime(utcNow.Year, utcNow.Month, utcNow.Day, 0, 0, 0, DateTimeKind.Utc);
                 
                 await Task.Run(() => _powerPlant.Calculate.RadiationOnTiltedPanel(dateTime));
-                
-                //powerPlant.EnergyEarning = powerPlant.MaximumEnergy();
                 _powerPlant.PowerEarning = _powerPlant.PowerOverDay();
 
-                return new CreatedResult($"/Powerplant/Data", JsonConvert.SerializeObject(_powerPlant.PowerEarning));
+                return new CreatedResult(@$"/Powerplants/{plantId}/Power/ForDay?day={day}", 
+                    JsonConvert.SerializeObject(_powerPlant.PowerEarning));
             }
             catch (Exception e)
             {
-                var error = $"Cannot calculate power plant data: {e}";
+                var error = $"Cannot calculate power for a day: {e}";
                 _logger.LogError(error);
 
                 return Conflict(error);
             }
+        }
+
+        /// <summary>
+        /// Process day date - use UTC midnight
+        /// </summary>
+        /// <param name="day">for this day</param>
+        /// <returns>day at utc midnight</returns>
+        private DateTime ProcessDate(DateTime? day)
+        {
+            var utcNow = DateTime.Now.ToUniversalTime();
+                
+            return day.HasValue ? 
+                new DateTime(day.Value.Year, day.Value.Month, day.Value.Day, 0, 0, 0, DateTimeKind.Utc) : 
+                new DateTime(utcNow.Year, utcNow.Month, utcNow.Day, 0, 0, 0, DateTimeKind.Utc);
         }
     }
 }
