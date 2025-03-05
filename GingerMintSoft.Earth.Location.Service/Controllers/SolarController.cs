4109df1a10239a0f50c4e5fa9d1692eb73c23a15
StanDeMan <StanislausE@gmx.de>
2025-03-05 21:04:15 +0100

    First usable controller attempt after cleansing.

--- GingerMintSoft.Earth.Location.Service/Controllers/SolarController.cs
+++ GingerMintSoft.Earth.Location.Service/Controllers/SolarController.cs
@@ -6,69 +6,77 @@ using Newtonsoft.Json;
 namespace GingerMintSoft.Earth.Location.Service.Controllers
 {
     [Route("Solar")]
-    public class SolarController(IFileStore plantStore, ILogger<SolarController> logger) : Controller
+    public class SolarController(
+        IFileStore plantStore, 
+        ILogger<SolarController> logger) : Controller
     {
-        private readonly IFileStore _plantStore = plantStore;
-        private readonly ILogger<SolarController> _logger = logger;
-
         private PowerPlant? _powerPlant;
 
+        /// <summary>
+        /// Request power plant data by plantId
+        /// </summary>
+        /// <param name="plantId">For this plant</param>
+        /// <returns>Power Plant data</returns>
         [HttpGet]
         [Route("Powerplants/{plantId}")]
-        public async Task<ActionResult> PostPowerplant(string plantId)
+        public async Task<ActionResult> GetPowerplant(string plantId)
         {
             try
             {
-                return new CreatedResult(@$"/Powerplants/{plantId}", 
-                    await _plantStore.ReadByPlantIdAsync(plantId));
+                return new CreatedResult($"/Powerplants/{plantId}", 
+                    await plantStore.ReadByPlantIdAsync(plantId));
             }
             catch (Exception e)
             {
                 var error = $"Cannot read power plant: {e}";
-                _logger.LogError(error);
+                logger.LogError(error);
 
                 return Conflict(error);
             }
         }
 
+        /// <summary>
+        /// Request power plant energy for a day
+        /// </summary>
+        /// <param name="plantId">For this plant</param>
+        /// <param name="day">For this day</param>
+        /// <returns>Energy data per minute from sun rise till sun set</returns>
         [HttpGet]
         [Route("Powerplants/{plantId}/Energy/ForDay")]
         public async Task<ActionResult> GetEnergyForDay(string plantId, [FromQuery] DateTime? day)
         {
             try
             {
-                _powerPlant = JsonConvert
-                    .DeserializeObject<PowerPlant>(JsonConvert
-                        .SerializeObject(await _plantStore.ReadByPlantIdAsync(plantId)));
-
-                _powerPlant.ExecCalculation();
+                _powerPlant = await InitializePowerPlant(plantId);
 
                 await Task.Run(() => _powerPlant.Calculate.RadiationOnTiltedPanel(ProcessDate(day)));
                 _powerPlant.EnergyEarning = _powerPlant.EnergyOverDay();
 
-                return new CreatedResult($@"Powerplants/{plantId}/Energy/ForDay?day={day}", 
+                return new CreatedResult($"Powerplants/{plantId}/Energy/ForDay?day={day}", 
                     JsonConvert.SerializeObject(_powerPlant.EnergyEarning));
             }
             catch (Exception e)
             {
                 var error = $"Cannot calculate energy for a day: {e}";
-                _logger.LogError(error);
+                logger.LogError(error);
 
                 return Conflict(error);
             }
         }
 
+        /// <summary>
+        /// Request power plant power data for a day
+        /// </summary>
+        /// <param name="plantId">For this plant</param>
+        /// <param name="day">For this day</param>
+        /// <returns>Power data per minute from sun rise till sun set</returns>
         [HttpGet]
         [Route("Powerplants/{plantId}/Power/ForDay")]
         public async Task<ActionResult> GetPowerForDay(string plantId, [FromQuery] DateTime? day)
         {
             try
             {
-                _powerPlant = JsonConvert
-                    .DeserializeObject<PowerPlant>(JsonConvert
-                        .SerializeObject(await _plantStore.ReadByPlantIdAsync(plantId)));
-
-                _powerPlant.ExecCalculation();
+                _powerPlant = await InitializePowerPlant(plantId);
 
                 var utcNow = DateTime.Now.ToUniversalTime();
                 
@@ -79,16 +87,32 @@ namespace GingerMintSoft.Earth.Location.Service.Controllers
                 await Task.Run(() => _powerPlant.Calculate.RadiationOnTiltedPanel(dateTime));
                 _powerPlant.PowerEarning = _powerPlant.PowerOverDay();
 
-                return new CreatedResult(@$"/Powerplants/{plantId}/Power/ForDay?day={day}", 
+                return new CreatedResult($"/Powerplants/{plantId}/Power/ForDay?day={day}", 
                     JsonConvert.SerializeObject(_powerPlant.PowerEarning));
             }
             catch (Exception e)
             {
                 var error = $"Cannot calculate power for a day: {e}";
-                _logger.LogError(error);
+                logger.LogError(error);
 
                 return Conflict(error);
             }
+        }
+
+        /// <summary>
+        /// Initialize power plant
+        /// </summary>
+        /// <param name="plantId">For this plant</param>
+        /// <returns>Initialized power plant</returns>
+        private async Task<PowerPlant> InitializePowerPlant(string plantId)
+        {
+            var powerPlant = JsonConvert
+                .DeserializeObject<PowerPlant>(JsonConvert
+                    .SerializeObject(await plantStore.ReadByPlantIdAsync(plantId)));
+            
+            powerPlant.Initialize();
+
+            return powerPlant;
         }
 
         /// <summary>
