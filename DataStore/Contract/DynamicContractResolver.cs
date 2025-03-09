b8631f31e648d11d58f21848599c17ee1d1f42c1
StanDeMan <StanislausE@gmx.de>
2025-03-09 19:24:53 +0100

    Exclude properties at serialization

---
+++ DataStore/Contract/DynamicContractResolver.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace DataBase.Contract
+{
+    public class DynamicContractResolver(params string[] prop) : DefaultContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization) 
+        {
+            // return all the properties which are not in the ignore list
+            return base
+                .CreateProperties(type, memberSerialization)
+                .Where(p => !prop.Contains(p.PropertyName))
+                .ToList();
+        }
+    }
+}
