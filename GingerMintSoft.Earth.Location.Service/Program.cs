5d8c223713a943117f19d2c391a5b02df9ee30ae
StanDeMan <StanislausE@gmx.de>
2025-03-16 18:19:42 +0100

    Add cors because browser do not know localhost anymore

--- GingerMintSoft.Earth.Location.Service/Program.cs
+++ GingerMintSoft.Earth.Location.Service/Program.cs
@@ -15,8 +15,21 @@ namespace GingerMintSoft.Earth.Location.Service
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
             builder.Services.AddSingleton<IFileStore, FileStore>();
+            builder.Services.AddCors(options =>  
+            {  
+      
+                options.AddDefaultPolicy(  
+                    policy =>  
+                    {  
+                        policy.WithOrigins("https://localhost:7131", "http://localhost:5083")  
+                            .AllowAnyHeader() 
+                            .AllowAnyOrigin()
+                            .AllowAnyMethod();  
+                    });  
+            });  
 
             var app = builder.Build();
+            app.UseCors();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
